b4e4d8198ef76499957ae4a619b32f91c1362b97
Guilherme Abreu <guilhermesabreu08@gmail.com>
2025-08-12 23:06:47 -0300

    feat: add ex040_AprovacaoAlunos.py

---
+++ ex040_AprovacaoAlunos.py
@@ -0,0 +1,17 @@
+# Aula 12: Condições Aninhadas
+# Crie um programa que leia duas notas de um aluno e calcule sua média, mostrando uma mensagem no final, de acordo com a média atingida:
+# - Média abaixo de 5.0: REPROVADO
+# - Média entre 5.0 e 6.9: RECUPERAÇÃO
+# - Média 7.0 ou superior: APROVADO
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+media = (n1 + n2) / 2
+
+if media >= 7.0:
+    print('Parabéns você passou de ano!!')
+
+if media in [5.0, 6.9]:
+    print('Infelizmente você ficou de recuperação')
+
+if media < 5.0:
+    print('Infelizmente você foi REPROVADO')
