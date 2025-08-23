22d4f3497a094059953fecc3f7396178638aab73
Guilherme Abreu <guilhermesabreu08@gmail.com>
2025-08-22 23:47:56 -0300

    feat: add ex047_NumerosPares1a50.py

---
+++ Aula 13 - Estrutura de repeticao for/ex047_NumerosPares1a50.py
@@ -0,0 +1,5 @@
+# Aula 13: Estrutura de repetição for
+# Crie um programa que mostre na tela todos os números pares que estão no intervalo entre 1 e 50
+for c in range(2, 51, 2):
+    print(c, end=' ')
+print('Acabou')
